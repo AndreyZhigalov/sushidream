097cb7f14960f465d024b484995ab95a509bede8
Andrey Zhigalov <zigalov.andrey@gmail.com>
2024-04-11 03:51:06 +0300

    Updates

--- static/js/Loyalty.d749b0d2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkplanetsushi=self.webpackChunkplanetsushi||[]).push([[384],{4097:function(e,t,n){n.r(t);n(2791);var s=n(184);t.default=function(){return(0,s.jsx)("div",{style:{display:"flex",justifyContent:"center",alignItems:"center",height:"100vh",fontSize:"70px"},children:"Loyalty"})}}}]);
-//# sourceMappingURL=Loyalty.d749b0d2.chunk.js.map
